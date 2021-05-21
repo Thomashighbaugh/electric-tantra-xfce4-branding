fb2b87165f89d312f80743cf7b23bb4ef62e1dcc
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-05-20 18:34:56 -0700

    once and for all

---
+++ README.md
@@ -0,0 +1,6 @@
+# the Electric Tantra Branding for XFCE4
+
+While the intention of the Electric Tantra Linux is demonstrating my AwesomeWM configuration, that is roughly fully featured and should not require another distro upon its final release and subsequent updates to it, I none the less have need to keep around a modified XFCE4 for situations where I need to do something GUI related and want a more seamless environment to use than is standard with XFCE4, which I employ the underpinnings of in my AwesomeWM configuration due to its functionality being consistent and polished compared to Gnome or Plasma. 
+
+## Branding Considerations 
+This configuration of the XML files underpinning XFCE4 as it is displayed on screen reduce the screen resolution occupied by bars and other distractions, reducing them to a single bottom bar that is more tolerable when large than the two two bars together are and provides a familiar look to windows users (which I care little about but still its there).In order to immediately cue my eye as to which DE it is, I have allowed the bar to different in color from my AwesomeWM configuration. 
--- install.sh
+++ install.sh
@@ -1,14 +1,42 @@
-#!/bin/env bash
-##########################################################
-## TITLE: install.sh
-##
-## DESCRIPTION: Installs the XFCE4 branding for Electric Tantra Linux with the custom GTK and icon themes to go with it
-##
-##
-## AUTHOR: Thomas Leon Highbaugh <admin@thomasleonhighbaugh.me>
-##########################################################
+#!/bin/bash
 
-cp -rvf XFCE/.config/* ~/.config
-sudo cp -rvf el-logo.png /usr/share/icons
-git clone https://github.com/Thomashighbaugh/Dhumavati-Theme && cd Dhumavati-Theme && ./install.sh && cd .. && rm -rf Dhumavati-Theme
-git clone https://github.com/Thomashighbaugh/chhinamasta-icon-theme && cp -rvf chhiamasta-icon-theme /usr/share/icons && rm -rf chhinamasta-icon-theme
+cr="$(tput setaf 1)"
+cg="$(tput setaf 2)"
+cy="$(tput setaf 3)"
+cm="$(tput setaf 5)"
+sb="$(tput bold)"
+sn="$(tput sgr0)"
+
+print() {
+    case "$1" in
+        t | title)
+            shift
+            printf "%s\n" "${sb}${cg}[###]$*${sn}"
+        ;;
+        s | step)
+            shift
+            printf "%s\n" "${sb}${cm}[===]$*${sn}"
+        ;;
+        e | error)
+            shift
+            printf "%s\n" "${sb}${cr}[!!!]$*${sn}"
+            exit 1
+        ;;
+        w | warning)
+            shift
+            printf "%s\n" "${sb}${cy}[:::]$*${sn}"
+        ;;
+        *)
+            printf '%s\n' "$*"
+        ;;
+    esac
+}
+
+print t "Electric Tantra Branding for XFCE4"
+
+print s "Making Directory"
+mkdir -p "$HOME"/.config/xfce4 
+print s "Copying contents of this repo to the directory"
+cp -rvf "$PWD"/* "$HOME"/.config/xfce4
+
+print w "Installation Completed"
